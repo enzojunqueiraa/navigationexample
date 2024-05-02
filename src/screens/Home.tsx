1cafd869f01e5353b2d1f5f940e6d97c44e2eebd
enzojunqueiraa <enzobjunqueira@gmail.com>
2024-05-02 15:10:27 -0300

    first commit

---
+++ src/screens/Home.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import Head from "../components/Head";
+import Footer from "../components/Footer";
+
+function Home(): React.JSX.Element {
+    return (
+        <View>
+            <Head />
+            <Text> 
+            LIBERA A INTERNET AI DONO CLAUDIO E DONO ALEX
+            
+            </Text>
+            <Footer />
+        </View>
+    );
+
+}
+export default Home;
+
---
+++ src/screens/Profile.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import Head from "../components/Head";
+import Footer from "../components/Footer";
+
+function Profile(): React.JSX.Element {
+    return (
+        <View>
+            <Head />
+           
+            <Footer />
+        </View>
+    );
+
+}
+export default Profile;
+
